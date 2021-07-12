522cbbd519414056b473f32fee777a35c28a2e5f
Mina Noshy <mina-noshy@outlook.com>
2021-07-11 23:22:14 -0700

    youstina

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -10,27 +10,23 @@ namespace TwoSum
         {
             var lst = TwoSum(new int[] { 2, 6, 3, 9, 3, 1, 8, 1, 4 }, 15);
 
-            foreach (var i in lst)
-                Console.WriteLine(i);
-
-            Console.ReadLine(); 
+            Console.WriteLine(string.Join("-", lst));
         }
 
         public static List<int> TwoSum(int[] arr, int sum)
             {
+
+                // method to get two sum index of array.
                 Dictionary<int, int> dict = new Dictionary<int, int>();
                 List<int> lst = new List<int>();
-
-                int index;
 
                 for (int i = 0; i < arr.Length; i++)
                 {
                     dict.Add(i, arr[i]);
                     if (dict.Values.Contains(sum - arr[i]))
                     {
-                        index = dict.FirstOrDefault(x => x.Value == (sum - arr[i])).Key;
                         lst.Add(i);
-                        lst.Add(index);
+                        lst.Add(dict.FirstOrDefault(x => x.Value == (sum - arr[i])).Key);
                     }
                 }
 
